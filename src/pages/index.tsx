70b56f40b607cb12128961ef024dc9a3199766f0
Yusaku <island.6.s93@gmail.com>
2023-07-09 21:15:15 +0900

    fix ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,144 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {useId, useState} from 'react';
+import {BaseInput} from 'src/parts/input';
 
-const inter = Inter({ subsets: ['latin'] })
+interface Languages {
+  ja: string;
+  en: string;
+  vi: string;
+}
 
 export default function Home() {
+  const japaneseId = useId();
+  const englishId = useId();
+  const vietnameseId = useId();
+
+  const [langArr, setLangArr] = useState<Languages[]>([
+    {
+      ja: '',
+      en: '',
+      vi: '',
+    },
+  ]);
+
+  const [lang, setLang] = useState('English');
+
+  function handleChangeLang(e: React.ChangeEvent<HTMLSelectElement>) {
+    setLang(e.target.value);
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    console.log();
+  }
+
+  function addRow(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    const newArr = [...langArr];
+    newArr.push({
+      ja: '',
+      en: '',
+      vi: '',
+    });
+
+    setLangArr(newArr);
+  }
+
+  function removeRow(index: number) {
+    const newArr = [...langArr];
+    newArr.splice(index, 1);
+
+    setLangArr(newArr);
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={`flex min-h-screen flex-col items-center justify-between p-24 `}>
+      <div>
+        <div className="pb-4">
+          <p>AMAZON TRANSLATE API</p>
         </div>
-      </div>
+        <div className="pb-4">
+          <label>Select language: </label>
+          <select value={lang} onChange={handleChangeLang} className="text-black w-40">
+            <option value="English">English</option>
+            <option value="Vietnamese">Vietnamese</option>
+          </select>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            {langArr.map((lang, index) => (
+              <div className="flex gap-4 pb-4 items-end" key={index}>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">日本語</label>
+                  <BaseInput
+                    value={langArr[index].ja}
+                    onChange={(e) => {
+                      const newArr = [...langArr];
+                      newArr[index].ja = e.target.value;
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                      setLangArr(newArr);
+                    }}
+                  />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">English</label>
+                  <BaseInput
+                    value={langArr[index].en}
+                    onChange={(e) => {
+                      const newArr = [...langArr];
+                      newArr[index].en = e.target.value;
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                      setLangArr(newArr);
+                    }}
+                  />
+                </div>
+                <div>
+                  <label className="block text-gray-700 text-sm font-bold mb-2">Vietnamese</label>
+                  <BaseInput
+                    value={langArr[index].vi}
+                    onChange={(e) => {
+                      const newArr = [...langArr];
+                      newArr[index].vi = e.target.value;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                      setLangArr(newArr);
+                    }}
+                  />
+                </div>
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white  py-2 px-4 rounded-full h-10"
+                  onClick={() => removeRow(index)}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+          <div className="flex justify-center gap-4 items-center">
+            <button
+              onClick={addRow}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              type="button"
+            >
+              ＋ add
+            </button>
+          </div>
+          <div className="flex justify-end gap-4  items-center">
+            {/* {isLoading ? (
+              <div className="flex justify-center" aria-label="読み込み中">
+                <div className="animate-spin h-6 w-6 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+              </div>
+            ) : null} */}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </main>
-  )
+  );
 }
